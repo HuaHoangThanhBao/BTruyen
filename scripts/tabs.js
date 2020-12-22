630352b387e3ff61fe8b73482fef857c160318cb
HuaHoangThanhBao <baohua01234@gmail.com>
2020-12-22 10:43:39 +0700

    Chỉnh sửa giao diện trang chủ lần 1

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -3,7 +3,7 @@ for (let i = 1; i < tabs.length; ++i) {
     tabs[i].style.display = "none";
 }
 
-var tablinks = document.getElementsByClassName("tabs_link");
+var tablinks = document.getElementsByClassName("tabs--link");
 for (let i = 0; i < tablinks.length; ++i) {
     tablinks[i].addEventListener('click', function () {
         for (const tab of tabs) {
