23f299f4145fe5f6c0b431e0f41ef58ae1b50541
HuaHoangThanhBao <baohua01234@gmail.com>
2021-03-26 20:21:06 +0700

    Sửa trang quản lý truyện

--- truyen/js/modal.js
+++ truyen/js/modal.js
@@ -1,11 +1,14 @@
 const update_btns = document.getElementsByClassName('update-btn');
 const add_btn = document.getElementById('add-btn');
+const ndt_update_btns = document.getElementsByClassName('control-btn');
 
 const modal_update = document.getElementById('modal-update');
 const modal_add = document.getElementById('modal-add');
+const modal_ndt_add = document.getElementById('modal-ndt-add');
 
 const closeBtnUpdate = document.getElementById('closeBtnUpdate');
 const closeBtnAdd = document.getElementById('closeBtnAdd');
+const closeBtnNDTAdd = document.getElementById('closeBtnNDTAdd');
 
 /*Update Btns*/
 for(let i = 0; i < update_btns.length; i++){
@@ -26,5 +29,17 @@ add_btn.addEventListener('click', function(){
 
 closeBtnAdd.addEventListener('click', function(){
     modal_add.classList.remove('enable');
+});
+/**/
+
+/*Noi dung truyen Button*/
+for(let i = 0; i < ndt_update_btns.length; i++){
+    ndt_update_btns[i].addEventListener('click', function(){
+        modal_ndt_add.classList.add('enable');
+    });
+}
+
+closeBtnNDTAdd.addEventListener('click', function(){
+    modal_ndt_add.classList.remove('enable');
 });
 /**/
