1c58b70498cc722b9a511ee640c8f441ca5f60f4
HuaHoangThanhBao <baohua01234@gmail.com>
2021-06-10 09:45:58 +0700

    [Cập nhật lại cấu trúc source code]

--- scripts/change-theme.js
+++
@@ -1,6 +0,0 @@
-const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
-
-toggleSwitch.addEventListener('change', function(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-});
